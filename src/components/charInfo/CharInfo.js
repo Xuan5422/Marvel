63b914e4864d7dfa68f55bbe23fc8e84fa4659e2
Alex.E <erenkov2010@ya.ru>
2022-12-10 17:23:47 +0300

    classComponent

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -12,7 +12,7 @@ class CharInfo extends Component {
         super(props);
         this.char = {};
         this.state = {
-            
+
             loading: false,
             error: false,
         }
@@ -23,7 +23,7 @@ class CharInfo extends Component {
 
     componentDidUpdate(prevProps, prevState) {
         if (this.props.currChar !== prevProps.currChar) this.updateChar(this.props.currChar)
-    }    
+    }
 
     onError = () => {
         this.setState({
@@ -47,28 +47,28 @@ class CharInfo extends Component {
 
     updateChar = (id) => {
         console.log(id);
-        if(!id) return;
+        if (!id) return;
         this.turnONspiner();
         this.offError();
-        
+
         this.marvelService
             .getCharacter(id)
             .then(resp => {
-                this.char = {...resp};
-                this.setState({loading: false, error: false});
+                this.char = { ...resp };
+                this.setState({ loading: false, error: false });
             })
             .catch(this.onError);
 
- //           this.foo.bar = 0;
+        //           this.foo.bar = 0;
     }
 
     render() {
 
-        const {loading, error} = this.state;
-        const {currChar} = this.props;
+        const { loading, error } = this.state;
+        const { currChar } = this.props;
 
-      //  const content = !(loading || error || !currChar) ? <View char={this.char}/> : null;
-        const content = !(loading || error) && currChar ? <View char={this.char}/> : null;
+        //  const content = !(loading || error || !currChar) ? <View char={this.char}/> : null;
+        const content = !(loading || error) && currChar ? <View char={this.char} /> : null;
         const spiner = loading ? <Spiner /> : null;
         const errorMesage = error ? <ErroeMessage /> : null;
         const skeleton = currChar ? null : <Skeleton />
@@ -85,78 +85,49 @@ class CharInfo extends Component {
 }
 
 
-const View = ({char}) => {
-    const {name, description, thumbnail, homepage, wiki, comics } = char;
+const View = ({ char }) => {
+    const { name, description, thumbnail, homepage, wiki, comics } = char;
     const descr = description ? description : "There is no description for this character.";
-    let imgStyle = {'objectFit': 'cover'};
+    let imgStyle = { 'objectFit': 'cover' };
     let comicsList;
 
-    if(thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') imgStyle = {'objectFit': 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') imgStyle = { 'objectFit': 'cover' };
 
-    if({...comics}.available) {
-           comicsList = {...comics}.items.map( (item, i) => {
-                return (
-                    <a key={i} className="char__comics-item" href={item.resourceURI}> {item.name}</a>
-                )
-                })
-        } else comicsList = "There are no comisc for this character.";
-    
- 
+    if ({ ...comics }.available) {
+        comicsList = { ...comics }.items.map((item, i) => {
+            return (
+                <a key={i} className="char__comics-item" href={item.resourceURI}> {item.name}</a>
+            )
+        })
+    } else comicsList = "There are no comisc for this character.";
+
+
     return (
-        <>
+        <div char__wrapp>
             <div className="char__basics">
-                    <img src={thumbnail} alt={name} style={imgStyle}/>
-                    <div>
-                        <div className="char__info-name">{name}</div>
-                        <div className="char__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
+                <img src={thumbnail} alt={name} style={imgStyle} />
+                <div>
+                    <div className="char__info-name">{name}</div>
+                    <div className="char__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">Wiki</div>
+                        </a>
                     </div>
                 </div>
-                <div className="char__descr">
-                    {descr}
-                </div>
-                <div className="char__comics">Comics:</div>
-                <ul className="char__comics-list">
-                    {comicsList}
-                    {/* <li className="char__comics-item">
-                        All-Winners Squad: Band of Heroes (2011) #3
-                    </li>
-                    <li className="char__comics-item">
-                        Alpha Flight (1983) #50
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing Spider-Man (1999) #503
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing Spider-Man (1999) #504
-                    </li>
-                    <li className="char__comics-item">
-                        AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Vengeance (2011) #4
-                    </li>
-                    <li className="char__comics-item">
-                        Avengers (1963) #1
-                    </li>
-                    <li className="char__comics-item">
-                        Avengers (1996) #1
-                    </li> */}
-                </ul>
-        </>
-        
+            </div>
+            <div className="char__descr">
+                {descr}
+            </div>
+            <div className="char__comics">Comics:</div>
+            <ul className="char__comics-list">
+                {comicsList}
+   
+            </ul>
+        </div>
+
     )
 }
 
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -33,7 +33,7 @@ class CharList extends Component {
             .getAllCharacters(this.state.offset)
                 .then((resp) => {
                     this.setState({
-                        charLst: resp,
+                        charLst: [...this.state.charLst, ...resp],
                         offset: this.state.offset + 9
                     });
                     currCharId(null);          
@@ -54,48 +54,25 @@ class CharList extends Component {
     render() {
         const {charLst, activCharItem} = this.state;
 
-        for (let i = 0; i <= 8; i++) this.arrClass[i] = "char__item";
+        for (let i = 0; i <= charLst.length; i++) this.arrClass[i] = "char__item";
         if(activCharItem !== null) this.arrClass[activCharItem] = `${this.arrClass[activCharItem]} char__item_selected`;
+
+        const visCharList = charLst.map( (item, i) => {
+            return (
+                <li tabIndex ="0" key={i} id={i} className={this.arrClass[i]} onFocus={this.onCharClick}>
+                <img src={{...item}.thumbnail} alt={{...item}.name} />
+                <div className="char__name">{{...item}.name}</div>
+            </li>
+            )
+            
+        })
 
        return (
             <div className="char__list">
                 <ul className="char__grid">
-                    <li tabindex ="0" id={"0"} className={this.arrClass[0]} onFocus={this.onCharClick}>
-                        <img src={{...charLst[0]}.thumbnail} alt="abyss" />
-                        <div className="char__name">{{...charLst[0]}.name}</div>
-                    </li>
-                    <li tabindex ="0" id="1" className={this.arrClass[1]} onFocus={this.onCharClick}>
-                        <img src={{...charLst[1]}.thumbnail} alt="abyss" />
-                        <div className="char__name">{{...charLst[1]}.name}</div>
-                    </li>
-                    <li tabindex ="0" id="2" className={this.arrClass[2]} onFocus={this.onCharClick}>
-                        <img src={{...charLst[2]}.thumbnail} alt="abyss" />
-                        <div className="char__name">{{...charLst[2]}.name}</div>
-                    </li>
-                    <li tabindex ="0" id="3" className={this.arrClass[3]} onFocus={this.onCharClick}>
-                        <img src={{...charLst[3]}.thumbnail} alt="abyss" />
-                        <div className="char__name">{{...charLst[3]}.name}</div>
-                    </li>
-                    <li tabindex ="0" id="4" className={this.arrClass[4]} onFocus={this.onCharClick}>
-                        <img src={{...charLst[4]}.thumbnail} alt="abyss" />
-                        <div className="char__name">{{...charLst[4]}.name}</div>
-                    </li>
-                    <li tabindex ="0" id="5" className={this.arrClass[5]} onFocus={this.onCharClick}>
-                        <img src={{...charLst[5]}.thumbnail} alt="abyss" />
-                        <div className="char__name">{{...charLst[5]}.name}</div>
-                    </li>
-                    <li tabindex ="0" id="6" className={this.arrClass[6]} onFocus={this.onCharClick}>
-                        <img src={{...charLst[6]}.thumbnail} alt="abyss" />
-                        <div className="char__name">{{...charLst[6]}.name}</div>
-                    </li>
-                    <li tabindex ="0" id="7" className={this.arrClass[7]} onFocus={this.onCharClick}>
-                        <img src={{...charLst[7]}.thumbnail} alt="abyss" />
-                        <div className="char__name">{{...charLst[7]}.name}</div>
-                    </li>
-                    <li tabindex ="0" id="8" className={this.arrClass[8]} onFocus={this.onCharClick}>
-                        <img src={{...charLst[8]}.thumbnail} alt="abyss" />
-                        <div className="char__name">{{...charLst[8]}.name}</div>
-                    </li>
+
+                    {visCharList}
+
                 </ul>
                 <button className="button button__main button__long" onClick={this.updateCharList} >
                     <div className="inner">load more</div>
